da0f85c609f0f8988f732038ff7fe5565f03f5e2
yaodong <wangyaodong0126@163.com>
2017-03-30 21:18:35 +0800

    yaodong :: resolve decode problem

--- test/helper/wsClient.js
+++ test/helper/wsClient.js
@@ -1,20 +1,22 @@
 const WebSocket = require('ws');
-const ws = new WebSocket('ws://127.0.0.1:8080');
+const userId = 1001;
+const ws = new WebSocket(`ws://127.0.0.1:8080/?userId=${userId}`);
+
+function send (ws, serverType, func, msg) {
+  msg = encode(msg);
+  ws.send(`${serverType}__${func}__${msg}`);
+}
+
+function encode (msg) {
+  return JSON.stringify(msg);
+}
 
 ws.on('open', () => {
   for (let i = 0; i < 2; i++) {
-    ws.send(JSON.stringify([
-      'chat', // server type
-      'talk', // function
-      ['hello world', ''] // args
-    ]));
+    send(ws, 'chat', 'talk', ['hello', 'world']);
   }
   for (let i = 0; i < 2; i++) {
-    ws.send(JSON.stringify([
-      'battle', // server type
-      'pk', // function
-      ['game over'] // args
-    ]));
+    send(ws, 'pk', 'fight', ['game over']);
   }
 });
 
